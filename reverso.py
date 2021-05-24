d5021f25bcf6b199d900a1c7d9e09a3ce711d1f3
Aamir Mirza <spookane112@gmail.com>
2021-05-24 17:10:25 -0400

    Made Reverso its own module

---
+++ reverso.py
@@ -0,0 +1,166 @@
+"""
+This is the class to handle communicating with the Reverso website.
+"""
+
+# TODO: Write a comment for each function
+import pyperclip
+import requests
+from fake_useragent import UserAgent
+from lxml import html
+
+
+def isNum(var):
+    try:
+        int(var)
+        return True
+    except ValueError:
+        return False
+
+
+def isSaying(definition):
+    sayings = ('au sens figuré', 'par extension', 'vieilli', 'familier',
+               'langage soutenu', "dans l'absolu", 'argot', 'armée',
+               'mot américain, armée', 'mot américain', 'péjorativement',
+               'populairement', 'ancien', 'Canada', 'Bélgique', 'mécanique',
+               'très familièrement', 'par plaisanterie', 'grossièrement')
+    if list(definition)[0] == '(' and list(definition)[-1] == ')':
+        return definition
+    for saying in sayings:
+        if saying in definition:
+            return str('(' + definition + ')')
+    return False
+
+
+def isReflexive(definition, word):
+    split = definition.split()
+    try:
+        if split[0] in ('se') and split[1] == word:
+            return True
+        elif list(word)[0] in ('a', 'e', 'i', 'o', 'u', 'h'):
+            split = definition.split("'")
+            if split[0] == "s" and split[1] == word:
+                return True
+    except IndexError:
+        pass
+    return False
+
+
+def isAlternative(word):
+    if list(word)[0] == ',':
+        return True
+    else:
+        return False
+
+
+def get_dictionary_definition(word, definition_list, definitions, indexes, sayings, alternatives):
+    print(definition_list)  # TODO: convert to logging
+    # TODO: Use new parameters instead of inference
+
+    if isAlternative(definition_list[0]):
+        definition_list = definition_list[1:]
+
+    numbered = False
+    definitions = {}
+
+    hold_num = 0
+    hold_saying = ''
+    reflexive = False
+    for definition in definition_list:
+        if isNum(definition) or isSaying(definition) or isReflexive(definition, word):
+            numbered = True
+            if isReflexive(definition, word):
+                reflexive = True
+                reflexive_term = definition
+                continue
+            if isSaying(definition):
+                hold_saying = isSaying(definition)
+                continue
+            if int(definition) > hold_num:
+                hold_num = int(definition)
+                print(hold_num)
+            else:
+                numbered = False
+                continue
+        else:
+            if numbered:
+                if hold_saying and not reflexive:
+                    if not hold_num:
+                        hold_num += 1
+                    definitions[str(hold_num)] = hold_saying + " " + definition
+                    numbered = False
+                    hold_saying = ''
+                    continue
+                if reflexive:
+                    if not hold_num:
+                        hold_num += 1
+                    if not hold_saying:
+                        definitions[str(hold_num)] = "({}) {}".format(
+                            reflexive_term,
+                            definition)
+                    else:
+                        definitions[str(hold_num)] = "({}) {} {}".format(
+                            reflexive_term,
+                            hold_saying,
+                            definition)
+                        hold_saying = ''
+                    numbered = False
+                    continue
+                definitions[str(hold_num)] = definition
+                numbered = False
+                continue
+            else:
+                if not hold_num:
+                    hold_num += 1
+                    definitions[str(hold_num)] = definition
+                else:
+                    break
+
+    return definitions
+
+
+class ReversoDictionary:
+    base_url = "http://mobile-dictionary.reverso.net/french-definition/"
+
+    def __init__(self):
+        self.headers = {'User-Agent': UserAgent(veryify_ssl=False).firefox}  # Needed to make requests
+
+    def _word_url(self, word):
+        """The URL for the specific word"""
+        return self.base_url + word
+
+    def _get_definition_page_html(self, word):
+        page = requests.get(self._word_url(word), headers=self.headers)
+        return html.fromstring(page.content)
+
+    def _get_word_definition_list(self, word):
+        definition_xpath = '//span[@direction="target"]/text()'
+        saying_xpath = '//span[@style="color:#008000;"]/text()'
+        index_numbers_xpath = '//span[@style="background-color:#000000"]/text()'
+        alternative_words_xpath = '//span[@style="color:#0000ff;"]/text()'
+        all_xpaths = f'{definition_xpath} | {saying_xpath} | {index_numbers_xpath} | {alternative_words_xpath}'
+
+        page_html = self._get_definition_page_html(word)
+        definitions = page_html.xpath(definition_xpath)
+        sayings = page_html.xpath(saying_xpath)
+        index_numbers = page_html.xpath(index_numbers_xpath)
+        alternative_words = page_html.xpath(alternative_words_xpath)
+        definition_list = page_html.xpath(all_xpaths)
+
+        return get_dictionary_definition(word, definition_list, definitions, index_numbers, sayings, alternative_words)
+
+    def get_word_definitions(self, word, *, copy_to_clipboard=False):
+        definitions = self._get_word_definition_list(word)
+
+        message = ''
+        print("\n" + word + "\n")  # TODO: convert to logging
+
+        starting_index = 1
+        for index in range(starting_index, len(definitions.keys()) + 1):
+            try:
+                message += '{} - {}\n'.format(index, definitions[str(index)])
+            except KeyError:
+                continue
+        print(message)  # TODO: Convert to logging
+
+        if copy_to_clipboard:
+            pyperclip.copy(message)
