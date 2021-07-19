4a94bc92bf5b5fe6d272ec1087697f0d9f189fb6
Aamir Mirza <spookane112@gmail.com>
2021-07-19 17:16:23 -0400

    Created functional test for retrieving the word definition

--- tests/test_reverso.py
+++ tests/test_reverso.py
@@ -1,3 +1,5 @@
+import pytest
+
 from reverso import ReversoDictionary
 
 
@@ -12,3 +14,10 @@ class TestReverso:
         mocker.patch('reverso.UserAgent')
         assert self.reverso._word_url("aléatoire") == \
                "https://mobile-dictionary.reverso.net/french-definition/al%C3%A9atoire"
+
+    @pytest.mark.slow
+    def test_retrieves_definition(self):
+        definitions = self.reverso._get_word_definition_list('danser')
+        assert definitions == {'1': "exécuter une danse, bouger son corps en suivant un rythme musical",
+                               '2': "onduler, se mouvoir en parlant de choses, d'animaux ou d'éléments naturels",
+                               '3': "(se danser) être dansé"}
